68fb981d6cbbcfa1f8ff3cbf103b0acbf61bf4a2
Aliaksandr Bazylchyk <bazylchyk.a@gmail.com>
2022-10-14 15:18:31 +0300

    Unsecured SQL request

--- Application/Controllers/HomeController.cs
+++ Application/Controllers/HomeController.cs
@@ -2,15 +2,10 @@
 using Application.Repository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.Sql;
 using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Newtonsoft.Json;
 using System.Data;
-using System.Data.Common;
 using System.Diagnostics;
-using System.Net;
-using System.Reflection;
 
 namespace Application.Controllers
 {
@@ -54,7 +49,7 @@ namespace Application.Controllers
                             break;
                         case "UnsecuredUserName":
                             viewModel.Filter = "UnsecuredUserName";
-                            //users = sqlRequest(searchString);
+                            users = sqlRequest(searchString);
                             break;
                     }
                 }
@@ -66,50 +61,40 @@ namespace Application.Controllers
             }
             return View();
         }
-        private IEnumerable<IdentityUser> sqlRequest(string searchString)
+        private List<IdentityUser> sqlRequest(string searchString)
         {
             List<IdentityUser> users = new List<IdentityUser>();
             var dt = new DataTable();
 
             string connectionString = "Server=(localdb)\\mssqllocaldb;Database=aspnet-Application-443ED096-10DB-49D1-97CD-BCFC4209136A;Trusted_Connection=True;MultipleActiveResultSets=true";
-            string sqlExpression = $"SELECT * FROM AspNetUsers WHERE UserName='{searchString}';";
+            string sqlExpression = $"SELECT * FROM AspNetUsers WHERE UserName = '{searchString}';";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 SqlCommand command = new SqlCommand(sqlExpression, connection);
                 var reader = command.ExecuteReader();
-
-                users = ConvertDataTable<IdentityUser>(dt);
+                dt.Load(reader);
+                string json = JsonConvert.SerializeObject(dt);
+                users = JsonConvert.DeserializeObject<List<IdentityUser>>(json);
             }
+
             return users;
         }
 
-        private static List<T> ConvertDataTable<T>(DataTable dt)
+        public IEnumerable<T> GetData<T>(IDataReader reader, Func<IDataRecord, T> BuildObject)
         {
-            List<T> data = new List<T>();
-            foreach (DataRow row in dt.Rows)
+            try
             {
-                T item = GetItem<T>(row);
-                data.Add(item);
-            }
-            return data;
-        }
-        private static T GetItem<T>(DataRow dr)
-        {
-            Type temp = typeof(T);
-            T obj = Activator.CreateInstance<T>();
-            foreach (DataColumn column in dr.Table.Columns)
-            {
-                foreach (PropertyInfo pro in temp.GetProperties())
+                while (reader.Read())
                 {
-                    if (pro.Name == column.ColumnName)
-                        pro.SetValue(obj, dr[column.ColumnName], null);
-                    else
-                        continue;
+                    yield return BuildObject(reader);
                 }
             }
-            return obj;
+            finally
+            {
+                reader.Dispose();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
